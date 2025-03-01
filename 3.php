6c66c079e2ce76f5f6513a9c4530e3dac57b4921
12345 <alexandrohotnikov000@gmail.com>
2025-03-01 16:51:12 +0300

    == -> ===

--- 3.php
+++ 3.php
@@ -3,9 +3,9 @@ $month = readline("Введите порядковый номер месяца: 
 if ($month < 1 || $month > 12) {
     echo "Ошибка: введите число от 1 до 12";
 } else {
-    if ($month == 2) {
+    if ($month === 2) {
         $days = 28;
-    } elseif ($month == 4 || $month == 6 || $month == 9 || $month == 11) {
+    } elseif ($month === 4 || $month === 6 || $month === 9 || $month === 11) {
         $days = 30;
     } else {
         $days = 31;
